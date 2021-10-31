e490bc500bb5bd1e4c0def34021fdec5ee10f011
dimon-mirchuk <dimon.mirchuk@gmail.com>
2021-10-31 21:04:14 +0300

    intermediate commit

---
+++ src/Service/ServiceAPI.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const BASE_URL = "https://pixabay.com/api/";
+const API_KEY = "23364798-13e999d1bd4f00c21ed04ef58";
+
+export const getImages = (name, page) =>
+  axios.get(
+    `${BASE_URL}?q=${name}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
+  );
--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,15 @@
 import { Component } from "react";
 import "./App.css";
+import * as api from "../Service/ServiceAPI";
+import SearchBar from "./SearchBar";
+
+const INITIAL_STATE = { page: 1, picture: [] };
 
 class App extends Component {
-  state = {};
+  state = {
+    ...INITIAL_STATE,
+    isLoading: false,
+  };
 
   render() {
     return <div></div>;
